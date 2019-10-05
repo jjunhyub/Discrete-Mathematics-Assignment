fd56be354a2b17feac6de54db0cb15b722a970a1
Junhyub Lee <jjunhyub3090@gmail.com>
2019-10-05 19:25:35 +0900

    20162874_assignment_02

---
+++ 20162874_03/20162874_03.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    char arr[11];
+    for(int i = 1;i<argc;i++)arr[i] = argv[i][0];
+    int num = 1<<(argc-1);
+    for(int i = 0;i<num;i++)
+    {
+        printf("[%04d] : ",i+1);
+        if(!i)
+        {
+            printf("%c\n",155);
+            continue;
+        }
+        bool check = true;
+        for(int j = 1, temp = i;temp>0;temp = temp>>1,j++)
+        {
+            if(temp&1)
+            {
+                if(check)
+                {
+                    cout<<arr[j];
+                    check = !check;
+                }
+                else cout<<", "<<arr[j];
+            }
+        }
+        cout<<"\n";
+    }
+}
